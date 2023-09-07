ac28dc369956d1572521292724d354e5bdbf4b17
Ngan2302 <144303204+Ngan2302@users.noreply.github.com>
2023-09-07 14:40:17 +1000

    Update week4.js

--- week4.js
+++ week4.js
@@ -1,3 +1,18 @@
-/*
+/*function setup() {
+  createCanvas(400, 400);}
+function draw() {
+  background("white");
+  
+  fill("#fae");
+  circle(200, 280, 100);
+   circle(200, 100, 100);
+  
+  fill("grey")
+ circle(110, 200, 70);
+ circle(290, 200, 70);
+  
+  fill("rgba(0,255,0, 0.25)")
+ circle(200, 200, 170);
+}
     Paste the code for your week 4 exercise below.
 */
