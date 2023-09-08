704a68eaafcb8199fdcabc3328820ef1f0c3dc58
Ngan2302 <144303204+Ngan2302@users.noreply.github.com>
2023-09-08 14:12:24 +1000

    Update week6.js

--- week6.js
+++ week6.js
@@ -1,3 +1,29 @@
 /*
-    Paste the code for your week 3 exercise below.
+    Paste the codfunction setup() {
+  createCanvas(300, 300);
+}
+
+function draw() {
+  if (mouseIsPressed) {
+      fill(0, 255, 255);
+    circle(mouseX, mouseY, 25);
+  }}
+function mouseWheel() {
+  background(0, 255, 0);}
+function mouseClicked(){
+  background(32);
+}
+function keyPressed(){
+ if (key == 'r') {
+      background("red");
+    }
+    else if (key == 'y') {
+      background("yellow");
+    }
+    else if (key == 'p') {
+      background("#fae");
+    }  
+    else if (key == 'u') {
+      background("blue");
+    }  e for your week 3 exercise below.
 */
