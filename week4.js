afe2e3665418aa6c6bc5771c67a1419fb4c3ce46
Ngan2302 <144303204+Ngan2302@users.noreply.github.com>
2023-09-15 13:41:07 +1000

    Update week4.js

--- week4.js
+++ week4.js
@@ -1,18 +1,33 @@
-/*function setup() {
+function setup() {
   createCanvas(400, 400);}
 function draw() {
-  background("white");
+  colorMode(HSB);
+background(205, 54, 100);
+
+
+    fill("green");
+  ellipse(195, 260, 200, 55);
   
+  fill("yellow");
+rect(120, 160, 150, 100);
+  
+fill("red");
+triangle(110, 150, 150, 85, 280, 150);  
+
   fill("#fae");
-  circle(200, 280, 100);
-   circle(200, 100, 100);
+triangle(110, 150, 250, 100, 280, 150); 
   
-  fill("grey")
- circle(110, 200, 70);
- circle(290, 200, 70);
+  fill("brown");
+  rect(180, 210, 20, 50);
   
-  fill("rgba(0,255,0, 0.25)")
- circle(200, 200, 170);
-}
-    Paste the code for your week 4 exercise below.
-*/
+    fill("white");
+ rect(130, 170, 30, 30);
+   rect(220, 170, 30, 30);
+  
+fill("black");
+  textFont("Courier");
+  textSize(50);
+  text('Home', 130, 340);
+
+
+
