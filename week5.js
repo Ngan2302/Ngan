86e3a6f35c626f8c86bd65c2ee37d6deff214744
Ngan2302 <144303204+Ngan2302@users.noreply.github.com>
2023-09-17 19:07:36 +1000

    Update week5.js

--- week5.js
+++ week5.js
@@ -1,3 +1,18 @@
-/*
-    Paste the code for your week 5 exercise below.
-*/
+function setup() {
+  createCanvas(500, 500, WEBGL);
+}
+function draw() {
+  background(255);
+  
+  rotateY(millis() / 1000);
+  box(100);
+  circle(0, -70, 90);
+  
+  translate(0, 100);
+  rotateY(millis() / 1000);
+  cone(70, 100);
+  
+  translate(170, -150);
+  rotateZ(millis() / 1000);
+triangle(20, 80, 50, 20, 75, 80);  
+}
